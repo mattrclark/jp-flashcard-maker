8b7cb81c5ed0648c8061593f77c87db0957c8752
Matthew Clark <matrclark@gmail.com>
2021-08-06 23:18:05 +0100

    Add page creation for flashcard sets

---
+++ flashcardBookCreator.py
@@ -0,0 +1,54 @@
+from PIL import Image
+from datetime import datetime
+import helper, os
+
+class FlashcardBookCreator:
+    def __init__(self):
+        self.WIDTH = 2480
+        self.HEIGHT = 3508
+        self.currentX = 0
+        self.currentY = 0
+        self.pageIndex = 0
+        self.padding = 0
+
+    def createPages(self, images):
+        directory = "output/flashcard-books/book-{0}".format(datetime.now().strftime("%d-%m-%Y-%H-%M-%S"))
+        os.makedirs(directory)
+
+        # Create a page
+        page = self.__createPage()
+
+        # Loop through all images
+        helper.printProgressBar(0, len(images), prefix = 'Pages:', suffix = 'Complete - {0} Pages Created'.format(self.pageIndex), length = 50)
+        for i in range(0, len(images)):
+            img = images[i]
+            helper.printProgressBar(i + 1, len(images), prefix = 'Pages:', suffix = 'Complete - {0} Pages Created'.format(self.pageIndex), length = 50)
+
+            img_w, img_h = img.size
+
+            # Check if image is out of bounds
+            # If so, then correct (Create page or move down)
+            if(self.currentY + img_h + self.padding >= self.HEIGHT):
+                page.save("{1}/page-{0}.png".format(self.pageIndex, directory))
+                page = self.__createPage()
+            elif(self.currentX + img_w + self.padding >= self.WIDTH):
+                self.currentX = 0
+                self.currentY += img_h + self.padding
+                if(self.currentY + img_h + self.padding >= self.HEIGHT):
+                    page.save("{1}/page-{0}.png".format(self.pageIndex, directory))
+                    page = self.__createPage()
+            
+            # Paste images onto page
+            page.paste(img, (self.currentX, self.currentY))
+
+            # Move along to the next position
+            self.currentX += img_w + self.padding
+
+        # Save the page
+        page.save("{1}/page-{0}.png".format(self.pageIndex, directory))
+
+    def __createPage(self):
+        self.currentX = 0
+        self.currentY = 0
+        self.pageIndex += 1
+        return Image.new('RGBA', (self.WIDTH, self.HEIGHT), (255, 255, 255, 255))
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+from flashcardBookCreator import FlashcardBookCreator
 import qrcode, json, helper, csv
 from PIL import Image, ImageDraw, ImageFont
 from wanikani import Wanikani
 
 JISHO_WORD = "https://www.jisho.org/word/{0}"
-IMG_WIDTH = 1000
-IMG_HEIGHT = 600
+IMG_WIDTH = 826
+IMG_HEIGHT = 438
 LEVEL = 8
+SAVE_INDIVIDUAL_IMAGES = False
 
 def createQRCode(level, jp, eng, reading):
     # Make QR Code
     qrImg = qrcode.make(JISHO_WORD.format(jp))
+    qrImg = qrImg.resize((IMG_HEIGHT // 2, IMG_HEIGHT // 2), Image.NEAREST)
     qrImg_w, qrImg_h = qrImg.size
 
     # Create new image and paste QR code onto it
@@ -20,22 +23,34 @@ def createQRCode(level, jp, eng, reading):
 
     # Draw the vocabulary onto the image
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", 130)
+    font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", 100)
     draw_w, draw_h = draw.textsize(jp, font=font)
     draw.text((IMG_WIDTH / 2 - draw_w / 2, 10), jp, fill=(255,0,0), font=font)
+
+    font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", 10)
+    draw_w, draw_h = draw.textsize(eng, font=font)
+    draw.text((IMG_WIDTH - draw_w - 5, IMG_HEIGHT - draw_h - 5), eng, fill=(0,0,0), font=font)
 
     # Draw the reading onto the image
     if(reading != ""):
         pr = "({0})".format(reading)
-        font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", 50)
+        font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", 30)
         draw_w, draw_h = draw.textsize(pr, font=font)
-        draw.text((IMG_WIDTH / 2 - draw_w / 2, 150), pr, fill=(255,0,0), font=font)
+        draw.text((IMG_WIDTH / 2 - draw_w / 2, 140), pr, fill=(255,0,0), font=font)
+
+    draw.rectangle(((0, 0), (IMG_WIDTH - 1, IMG_HEIGHT - 1)), outline = "black")
 
     # Save the file
-    img.save('output/{0}/{0}-{1}-{2}.png'.format(level, eng, jp))
+    if(SAVE_INDIVIDUAL_IMAGES):
+        img.save('output/{0}/{0}-{1}-{2}.png'.format(level, eng, jp))
 
+    return img
 
+# Load images from WaniKani
 def loadFromWaniKani():
+    images = []
+    flashcardCreator = FlashcardBookCreator()
+
     # Setup WaniKani request and send for specified level
     wk = Wanikani()
     r = wk.getVocabForLevel(LEVEL)
@@ -46,21 +61,29 @@ def loadFromWaniKani():
     print("Total subjects: {0}".format(response["total_count"]))
 
     # Go through each vocab and create a QR code flashcard
-    helper.printProgressBar(0, response["total_count"], prefix = 'Progress:', suffix = 'Complete', length = 50)
+    helper.printProgressBar(0, response["total_count"], prefix = 'Images:', suffix = 'Complete', length = 50)
     for i in range(0, response["total_count"]):
         d = response["data"][i]["data"]
-        helper.printProgressBar(i + 1, response["total_count"], prefix = 'Progress:', suffix = 'Complete', length = 50)
-        createQRCode(LEVEL, d["characters"], d["meanings"][0]["meaning"], d["readings"][0]["reading"])
+        helper.printProgressBar(i + 1, response["total_count"], prefix = 'Images:', suffix = 'Complete', length = 50)
+        images.append(createQRCode(LEVEL, d["characters"], d["meanings"][0]["meaning"], d["readings"][0]["reading"]))
 
+    flashcardCreator.createPages(images)
+
+# Load images from Csv
 def loadFromCsv(filename):
+    images = []
+    flashcardCreator = FlashcardBookCreator()
+
     with open("data/{0}.csv".format(filename), newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         rows = list(reader)
         totalrows = len(rows)
-        helper.printProgressBar(0, totalrows, prefix = 'Progress:', suffix = 'Complete', length = 50)
+        helper.printProgressBar(0, totalrows, prefix = 'Images:', suffix = 'Complete', length = 50)
         for i, row in enumerate(rows):
-            helper.printProgressBar(i + 1, totalrows, prefix = 'Progress:', suffix = 'Complete', length = 50)
-            createQRCode(filename, row["Vocab"], row["Meaning"], row["Reading"])
+            helper.printProgressBar(i + 1, totalrows, prefix = 'Images:', suffix = 'Complete', length = 50)
+            images.append(createQRCode(filename, row["Vocab"], row["Meaning"], row["Reading"]))
+        
+        flashcardCreator.createPages(images)
 
 if __name__ == "__main__":
     loadFromCsv("household")
